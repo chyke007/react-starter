c21e0f20870497b7926ae8e8189f51a8ee24703f
Nwachukwu Chibuike <nwachukwuchibuike2012@gmail.com>
2020-07-21 18:13:51 +0100

    Works with state and immutability

--- src/app/index.js
+++ src/app/index.js
@@ -1,31 +1,79 @@
-import React from "react";
-import {render} from "react-dom";
+// import React from "react";
+// import { render } from "react-dom";
 
-import { User } from './components/User';
-import { Main } from './components/Main';
+// import { User } from "./components/User";
+// import { Main } from "./components/Main";
 
-class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            username: "Max"
-        };
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       username: "Max",
+//     };
+//   }
+
+//   changeUsername(newName) {
+//     this.setState({
+//       username: newName,
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="container">
+//         <Main changeUsername={this.changeUsername.bind(this)} />
+//         <User username={this.state.username} />
+//       </div>
+//     );
+//   }
+// }
+
+// render(<App />, window.document.getElementById("app"));
+
+import { createStore } from "redux";
+
+const initialState = {
+    result: 1,
+    username: "Max",
+    lastValues: [],
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD":
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload],
+            };
+            break;
+        case "SUBTRACT":
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload],
+            };
+            break;
     }
+    return state;
+};
+const store = createStore(reducer);
 
-    changeUsername(newName) {
-        this.setState({
-            username: newName
-        });
-    }
+store.subscribe(() => {
+    console.log("Store updated", store.getState());
+});
 
-    render() {
-        return (
-            <div className="container">
-                <Main changeUsername={this.changeUsername.bind(this)}/>
-                <User username={this.state.username}/>
-            </div>
-        );
-    }
-}
+store.dispatch({
+    type: "ADD",
+    payload: 100,
+});
 
-render(<App />, window.document.getElementById('app'));
+store.dispatch({
+    type: "ADD",
+    payload: 22,
+});
+
+store.dispatch({
+    type: "SUBTRACT",
+    payload: 80,
+});
