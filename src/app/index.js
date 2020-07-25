a505546984d125621a11b5b511e7075c86b7e0cd
Nwachukwu Chibuike <nwachukwuchibuike2012@gmail.com>
2020-07-25 07:47:50 +0100

    Setup boiler code

--- src/app/index.js
+++ src/app/index.js
@@ -1,79 +1,12 @@
-// import React from "react";
-// import { render } from "react-dom";
+import { render } from "react-dom";
+import React from "react";
+import { Provider } from "react-redux";
 
-// import { User } from "./components/User";
-// import { Main } from "./components/Main";
-
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: "Max",
-//     };
-//   }
-
-//   changeUsername(newName) {
-//     this.setState({
-//       username: newName,
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div className="container">
-//         <Main changeUsername={this.changeUsername.bind(this)} />
-//         <User username={this.state.username} />
-//       </div>
-//     );
-//   }
-// }
-
-// render(<App />, window.document.getElementById("app"));
-
-import { createStore } from "redux";
-
-const initialState = {
-    result: 1,
-    username: "Max",
-    lastValues: [],
-};
-
-const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case "ADD":
-            state = {
-                ...state,
-                result: state.result + action.payload,
-                lastValues: [...state.lastValues, action.payload],
-            };
-            break;
-        case "SUBTRACT":
-            state = {
-                ...state,
-                result: state.result - action.payload,
-                lastValues: [...state.lastValues, action.payload],
-            };
-            break;
-    }
-    return state;
-};
-const store = createStore(reducer);
-
-store.subscribe(() => {
-    console.log("Store updated", store.getState());
-});
-
-store.dispatch({
-    type: "ADD",
-    payload: 100,
-});
-
-store.dispatch({
-    type: "ADD",
-    payload: 22,
-});
-
-store.dispatch({
-    type: "SUBTRACT",
-    payload: 80,
-});
+import App from "./containers/App";
+import store from "./store";
+render(
+  <Provider store={store}>
+    <App at={23} />
+  </Provider>,
+  window.document.getElementById("app")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require("path");
+const webpack = require("webpack");
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
@@ -8,20 +9,23 @@ var config = {
     output: {
         path: DIST_DIR + "/app",
         filename: "bundle.js",
-        publicPath: "/app/"
+        publicPath: "/app/",
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
     },
     module: {
-        loaders: [
-            {
-                test: /\.js?/,
-                include: SRC_DIR,
-                loader: "babel-loader",
-                query: {
-                    presets: ["react", "es2015", "stage-2"]
-                }
-            }
-        ]
-    }
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ["babel-loader"],
+        }, ],
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    devServer: {
+        contentBase: DIST_DIR,
+        hot: true,
+    },
 };
 
 module.exports = config;
